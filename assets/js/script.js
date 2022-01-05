21f9decc54f8e1e86fbce2ca3c9595d5af43f985
Tracy Tran <tracytran1189@gmail.com>
2022-01-04 21:28:52 -0600

    fetch pricing api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
 var getProducts = function(products) {
-    var priceApiUrl ="https://api.kroger.com/v1/products";
+    var priceApiUrl =`https://api.kroger.com/v1/products?filter.term=${TERM}&filter.filter.zipCode.near=75082`;
     fetch(priceApiUrl).then(function(response) {
         console.log ("products");
     })
 };
 
 getProducts();
+
+var axios = require("axios").default;
+
+var options = {
+  method: 'GET',
+  url: 'https://tasty.p.rapidapi.com/recipes/list',
+  params: {from: '0', size: '20', tags: 'under_30_minutes'},
+  headers: {
+    'x-rapidapi-host': 'tasty.p.rapidapi.com',
+    'x-rapidapi-key': '152c498cf3msh0f5746cfa2f4dcfp1e760cjsn3620d249e86f'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
