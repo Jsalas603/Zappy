c8916dcbdb7957892ecfaf0eced1cb28bb45a6be
Jorge A Salinas Jr <asalinas212@outlook.com>
2022-01-06 15:26:16 -0600

    js and readme prep

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Login Page Logic
+// User signs in, username is stored in local storage
+// Logged in user cannot return to login page
+
+// Landing Page Logic
+// User selects budget, calories, and vegan options
+
+// Tasty API is queried for recipes
+// Kroger API is queried for ingredient pricing
